094e897e2ce5b26e0fe749c5c80c9f5099201f6e
HarshSrivastava11 <harshsrivastava0968@gmail.com>
2023-03-05 00:46:55 +0530

    java

---
+++ Operators.java
@@ -0,0 +1,25 @@
+public class Operators {
+    public static void main(String[] args) {
+        int num1 = 90;
+        int num2 = 100;
+        
+        // is equal to
+        System.out.println(num1 == num2);
+        
+        // is not equal to
+        System.out.println(num1 != num2);
+
+        // is greater than
+        System.out.println(num1 > num2);
+
+        // is less than
+        System.out.println(num1 < num2);
+
+        // is greater than or equal to
+        System.out.println(num1 >= num2);
+
+        // is less than equal to
+        System.out.println(num1 <= num2);
+    }
+    
+}
