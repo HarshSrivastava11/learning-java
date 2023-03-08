a4bf8723aaad222696de0498c39ec3162d0da7d3
HarshSrivastava11 <harshsrivastava0968@gmail.com>
2023-03-09 01:19:41 +0530

    java

---
+++ Binary.java
@@ -0,0 +1,16 @@
+public class Binary {
+    private static int toBinary(int i) {
+        return 0;
+    }
+    public static void main(String[] args){
+        for(int i = -5; i < 33; i++){
+            System.out.println(i + ": " + toBinary(i));
+            System.out.println(i);
+            //always another way
+            System.out.println(i + ": " + Integer.toBinaryString(i));
+        }
+    }
+
+
+    
+}
--- Variables.java
+++ Variables.java
@@ -4,6 +4,5 @@ public class Variables {
         int age = 20;
 
         System.out.println("I am " + age + " years old.");
-
     }
 }
